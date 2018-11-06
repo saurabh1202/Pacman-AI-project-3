c0c4de04f4b33a7d0d434c268420ff1cc31dad10
saurabh1202 <saurabh_nimb@yahoo.in>
2018-11-06 15:12:03 -0500

    Q-learning done

--- qlearningAgents.py
+++ qlearningAgents.py
@@ -43,6 +43,7 @@ class QLearningAgent(ReinforcementAgent):
         ReinforcementAgent.__init__(self, **args)
 
         "*** YOUR CODE HERE ***"
+        self.qvalues = util.Counter()
 
     def getQValue(self, state, action):
         """
@@ -51,7 +52,7 @@ class QLearningAgent(ReinforcementAgent):
           or the Q node value otherwise
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.qvalues[(state,action)]
 
 
     def computeValueFromQValues(self, state):
@@ -62,7 +63,10 @@ class QLearningAgent(ReinforcementAgent):
           terminal state, you should return a value of 0.0.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        count = util.Counter()
+        for act in self.getLegalActions(state): 
+          count[act] = self.getQValue(state, act)  
+        return count[count.argMax()]  #Getting max value from count
 
     def computeActionFromQValues(self, state):
         """
@@ -71,7 +75,16 @@ class QLearningAgent(ReinforcementAgent):
           you should return None.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        if len(self.getLegalActions(state)) == 0:  
+            return None
+        
+        bestval = self.computeValueFromQValues(state)  
+        bestact = []
+        for act in self.getLegalActions(state):  
+            if bestval == self.getQValue(state, act):
+                bestact.append(act)
+
+        return random.choice(bestact)
 
     def getAction(self, state):
         """
@@ -102,7 +115,8 @@ class QLearningAgent(ReinforcementAgent):
           it will be called on your behalf
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        samp = reward + self.discount * self.getValue(nextState)
+        self.qvalues[(state,action)] = (1 - self.alpha) * self.qvalues[(state,action)] + self.alpha * samp  #Q updates
 
     def getPolicy(self, state):
         return self.computeActionFromQValues(state)
